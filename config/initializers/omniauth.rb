abaea296d09f68e74bad2c10561b7d2ecf20b241
Prasanna <prasofty@gmail.com>
2011-05-02 15:02:48 +0530

    change tmp path

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {:scope => 'publish_stream,offline_access,email'}
     provider :twitter, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET
     provider :linked_in, LINKEDIN_API_KEY, LINKEDIN_SECRET_KEY
-    provider :open_id, OpenID::Store::Filesystem.new('/tmp') if RAILS_ENV != 'production'
-    provider :open_id, OpenID::Store::Filesystem.new("#{RAILS_ROOT}/tmp") if RAILS_ENV == 'production'
+    provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
 
 #ActionController::Dispatcher.middleware do
