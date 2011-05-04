a0a86a1c592281020bebcb08016a49ea73fe47fd
Prasanna <prasofty@gmail.com>
2011-05-04 12:47:16 +0530

    added index to claim mains

--- db/migrate/20110503055009_addindextoclaimmains.rb
+++ db/migrate/20110503055009_addindextoclaimmains.rb
@@ -1,7 +1,17 @@
 class Addindextoclaimmains < ActiveRecord::Migration
   def self.up
+    add_index :claim_mains, :lastname
+    add_index :claim_mains, :firstname
+    add_index :claim_mains, :city    
+    add_index :claim_mains, :state
+    add_index :claim_mains, :zip
   end
 
   def self.down
+    remove_index :claim_mains, :lastname
+    remove_index :claim_mains, :firstname
+    remove_index :claim_mains, :city    
+    remove_index :claim_mains, :state
+    remove_index :claim_mains, :zip
   end
 end
